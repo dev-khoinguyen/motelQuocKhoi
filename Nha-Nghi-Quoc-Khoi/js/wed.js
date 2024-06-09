8d4cb4809aed7ddda66c890a5eaa6c86b0bf0afb
Nguyen Van Khoi <khoinv.it@gmail.com>
2024-06-09 20:08:50 +0700

    push all motelQuocKhoi project

---
+++ Nha-Nghi-Quoc-Khoi/js/wed.js
@@ -0,0 +1,4 @@
+alert("chào mừng quý anh/chị cô/bác đến với trang web của Nhà Nghỉ Quốc Khởi!");
+function onClick() {
+  alert("quý anh/chị cô/bác vui lòng liên hệ qua sđt sau để đặt phòng: 0814223339 (Nhiên)");
+}
